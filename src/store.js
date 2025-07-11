5032cff9f27cc98206d7bdc78ee6b8f369731583
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-07-11 20:34:50 +0530

    Meal slice and state integration

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 // store.js
 import { configureStore } from "@reduxjs/toolkit";
 import avReducer from "./avSlice";
+import mealsReducer from "./mealsSlice";
 import venueReducer from "./venueSlice";
 
 export default configureStore({
 	reducer: {
 		venue: venueReducer,
 		av: avReducer,
+		meals: mealsReducer,
 	},
 });
